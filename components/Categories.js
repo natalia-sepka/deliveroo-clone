98d587647e00614b93343621a1bbffe1c1d2894e
natalia.sepka <natalia.sepka@gmail.com>
2024-03-27 14:08:51 +0100

    Categories and CategoryCard add.

---
+++ components/Categories.js
@@ -0,0 +1,22 @@
+import {ScrollView} from 'react-native';
+import React from 'react';
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+    return (
+        <ScrollView
+            contentContainerStyle={{
+                paddingHorizontal: 15,
+                paddingTop: 10,
+            }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+        >
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing1' />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing2' />
+            <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing3' />
+
+        </ScrollView>
+    )
+}
+export default Categories;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import {Image, Text, TextInput, View} from 'react-native';
+import {Image, ScrollView, Text, TextInput, View} from 'react-native';
 import React, {useLayoutEffect} from 'react';
 import {useNavigation} from "@react-navigation/native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {AdjustmentsVerticalIcon, ChevronDownIcon, MagnifyingGlassIcon, UserIcon} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -40,6 +41,11 @@ const HomeScreen = () => {
                     </View>
                     <AdjustmentsVerticalIcon color="#00CCBB" />
                 </View>
+            {/*Body*/}
+            <ScrollView className="bg-gray-100">
+              <Categories />
+              {/*  featured rows*/}
+            </ScrollView>
         </SafeAreaView>
     )
 }
